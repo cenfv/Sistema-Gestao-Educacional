d39bfeb4b7245f8a06b17ee382cf36739b1a2c1f
cenfv <59135587+cenfv@users.noreply.github.com>
2023-01-28 10:06:36 -0300

    Inicializada página de criação de matérias

--- frontend/src/views/QuestionView.vue
+++ frontend/src/views/QuestionView.vue
@@ -1,85 +1,78 @@
 <template class="min-h-screen">
-  
+  <div class="bg-white max-w-6xl mx-auto rounded-lg p-10 shadow-md space-y-2">
+    <h1 class="text-3xl font-bold text-gray-900">Adicionar questão</h1>
+    <p class="text-lg font-normal text-gray-900">
+      Crie questões para as suas provas.
+    </p>
+
+    <div>
+      <h3 class="text-2xl font-bold text-gray-900">Cadastrar nova questão:</h3>
+    </div>
+
+    <form
+      @submit.prevent="handleCreateQuestion"
+      class="flex flex-col space-y-3"
+    >
+      <input
+        v-model="title"
+        class="bg-white rounded-lg p-4 drop-shadow-lg focus:outline-none focus:ring"
+        placeholder="Título da questão"
+        name="title"
+      />
+      <textarea
+        v-model="description"
+        placeholder="Enunciado da questão"
+        class="bg-white rounded-lg p-4 drop-shadow-lg focus:outline-none focus:ring"
+      ></textarea>
+
       <div
-        class="bg-white max-w-6xl mx-auto rounded-lg p-10 shadow-md space-y-2"
+        v-for="alternative in alternatives"
+        :key="alternative.id"
+        class="flex justify-center items-center"
       >
-        <h1 class="text-3xl font-bold text-gray-900">Adicionar questão</h1>
-        <p class="text-lg font-normal text-gray-900">
-          Crie questões para as suas provas.
-        </p>
+        <input
+          v-model="alternative.description"
+          placeholder="Descrição da alternativa"
+          class="bg-white rounded-lg p-4 drop-shadow-lg min-w-full focus:outline-none focus:ring"
+        />
 
-        <div>
-          <h3 class="text-2xl font-bold text-gray-900">Criar nova questão:</h3>
-        </div>
-
-        <form
-          @submit.prevent="handleCreateQuestion"
-          class="flex flex-col space-y-3"
+        <div
+          class="flex items-center cursor-pointer"
+          @click="handleRemoveAlternative(alternative.id)"
         >
-          <input
-            v-model="title"
-            class="bg-white rounded-lg p-4 drop-shadow-lg focus:outline-none focus:ring"
-            placeholder="Título da questão"
-            name="title"
-          />
-          <textarea
-            v-model="description"
-            placeholder="Enunciado da questão"
-            class="bg-white rounded-lg p-4 drop-shadow-lg focus:outline-none focus:ring"
-          ></textarea>
-
-          <div
-            v-for="alternative in alternatives"
-            :key="alternative.id"
-            class="flex justify-center items-center"
-          >
-            <input
-              v-model="alternative.description"
-              placeholder="Descrição da alternativa"
-              class="bg-white rounded-lg p-4 drop-shadow-lg min-w-full focus:outline-none focus:ring"
-            />
-
-            <div
-              class="flex items-center cursor-pointer"
-              @click="handleRemoveAlternative(alternative.id)"
-            >
-              <font-awesome-icon
-                class="absolute -ml-6"
-                icon="fa-solid fa-trash"
-              />
-            </div>
-          </div>
-          <button
-            @click="handleAddAlternative"
-            class="font-medium m-2 self-start text-blue-500"
-            type="button"
-          >
-            + Adicionar alternativa
-          </button>
-          <select
-            class="bg-white rounded-lg p-4 drop-shadow-lg focus:outline-none focus:ring"
-            v-model="selectedAlternative"
-          >
-            <option value="" disabled>Selecione a alternativa correta</option>
-            <option
-              v-for="alternative in alternatives"
-              :value="alternative.id"
-              :key="alternative.id"
-            >
-              {{ alternative.description }}
-            </option>
-          </select>
-
-          <div className="flex justify-end">
-            <button
-              className="mt-3 w-36  py-4  text-white font-medium rounded-lg text-center drop-shadow-lg 5 bg-blue-500 hover:bg-blue-700 "
-            >
-              Salvar
-            </button>
-          </div>
-        </form>
+          <font-awesome-icon class="absolute -ml-6" icon="fa-solid fa-trash" />
+        </div>
       </div>
-   
+      <button
+        @click="handleAddAlternative"
+        class="font-medium m-2 self-start text-blue-500"
+        type="button"
+      >
+        + Adicionar alternativa
+      </button>
+      <select
+        class="bg-white rounded-lg p-4 drop-shadow-lg focus:outline-none focus:ring"
+        v-model="selectedAlternative"
+      >
+        <option value="" disabled>Selecione a alternativa correta</option>
+        <option
+          v-for="alternative in alternatives"
+          :value="alternative.id"
+          :key="alternative.id"
+        >
+          {{ alternative.description }}
+        </option>
+      </select>
+
+      <div className="flex justify-end">
+        <button
+          className="mt-3 w-36  py-4  text-white font-medium rounded-lg text-center drop-shadow-lg 5 bg-blue-500 hover:bg-blue-700 "
+        >
+          Salvar
+        </button>
+      </div>
+    </form>
+  </div>
 </template>
 
 <script>
--- frontend/src/views/SubjectView.vue
+++ frontend/src/views/SubjectView.vue
@@ -1,12 +1,76 @@
-<template>
-    <div>
+<template class="min-h-screen">
+  <div class="bg-white max-w-6xl mx-auto rounded-lg p-10 shadow-md space-y-2">
+    <h1 class="text-3xl font-bold text-gray-900">Adicionar matéria</h1>
+    <p class="text-lg font-normal text-gray-900">
+      Crie matérias para seus alunos.
+    </p>
 
+    <div>
+      <h3 class="text-2xl font-bold text-gray-900">Cadastrar nova Matéria:</h3>
     </div>
+    <form
+      @submit.prevent="handleCreateQuestion"
+      class="flex flex-col space-y-3"
+    >
+      <input
+        v-model="name"
+        class="bg-white rounded-lg p-4 drop-shadow-lg focus:outline-none focus:ring"
+        placeholder="Nome da matéria"
+        name="name"
+      />
+      <select
+        class="bg-white rounded-lg p-4 drop-shadow-lg focus:outline-none focus:ring"
+        v-model="selectedStudents"
+        
+        multiple = "true"
+      >
+        <option value="" disabled>
+          Selecione os estudantes que participarão da matéria:
+        </option>
+        <option
+          v-for="student in students"
+          :value="student.id"
+          :key="student.id"
+        >
+          {{ student.name }}
+        </option>
+      </select>
+    </form>
+  </div>
 </template>
 
 <script>
-    export default {
-        
-    }
+import axios from "axios";
+export default {
+  data() {
+    return {
+      name: "",
+      students: [],
+      tests: [],
+      selectedStudents: [],
+    };
+  },
+  created() {
+    this.getStudents();
+    console.log(this.students);
+    //this.getTests();
+  },
+  methods: {
+    getStudents() {
+      axios
+        .get("http://localhost:3000/students", {
+          headers: {
+            authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+        })
+        .then((response) => {
+          console.log(response.data.student)
+          this.students = response.data.student;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
+};
 </script>
-
