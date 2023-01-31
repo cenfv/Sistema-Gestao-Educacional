f8fdf6ea6250fb32aeef80ea37b955d3e103a887
cenfv <59135587+cenfv@users.noreply.github.com>
2023-01-31 16:57:36 -0300

    Criação da página de home e contact

--- frontend/src/views/ContactView.vue
+++ frontend/src/views/ContactView.vue
@@ -1,5 +1,12 @@
 <template>
-    <div>
-      <h1>This is an contact page</h1>
-    </div>
-  </template>
+  <div class="mt-8 bg-gray-200 p-2 text-center border-8">
+    <h1 class="font-bold text-xl">Carlos Eduardo Nogueira de Freitas Veiga</h1>
+    <p class="font-semibold text-lg">
+      Estudante de Engenharia de Software na UTFPR, Campus Cornélio Procópio
+    </p>
+    <p class="font-semibold text-lg">Email: carlosnfreitasv@gmail.com</p>
+    <p class="font-semibold text-base">Telefone: (14) 99867-9438</p>
+    <p class="font-semibold text-base">Linkedin: Linkedin.com/in/cenfv</p>
+    <p class="font-semibold text-base">Github: Github.com/cenfv</p>
+  </div>
+</template>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,18 +1,40 @@
 <template>
-  <HelloWorld />
+  <div>
+    <div class="mt-14 sm:text-center lg:text-left">
+      <div class="tracking-tight font-extrabold text-gray-700 text-5xl">
+        <h1 class="block">Ferramenta para enriquecer</h1>
+        <h1 class="block text-gray-700 xl:inline">seus estudos</h1>
+      </div>
+      <p class="text-gray-500 max-w-xl mt-5 text-xl mx-0">
+        Descubra uma maneira inovadora de organizar e realizar as suas provas.
+        Conheça a nossa plataforma e dê o próximo passo para a sua aprovação!
+      </p>
+      <div class="flex space-x-2 mt-4">
+        <div class="flex">
+          <router-link
+            to="/login"
+            class="flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-gray-700 hover:bg-gray-900"
+          >
+            Começar agora!
+          </router-link>
+        </div>
+        <div class="flex">
+          <router-link
+            to=""
+            class="flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-gray-700 bg-gray-200 hover:bg-gray-400"
+          >
+            Sobre
+          </router-link>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import { defineComponent } from 'vue';
-
-// Components
-import HelloWorld from '../components/HelloWorld.vue';
+import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: 'HomeView',
-
-  components: {
-    HelloWorld,
-  },
+  name: "HomeView",
 });
 </script>
